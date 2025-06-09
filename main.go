76c19b2c4224200fddc172c0172b4b76de8c1344
prosphire <2198853596@qq.com>
2025-06-09 21:11:09 +0800

    Gin搭建Blog API's ( )（二）

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
-package main 
-import "github.com/gin-gonic/gin"
-func main(){
-	r:=gin.Default()
-	r.GET("/ping",func(ctx *gin.Context) {
-		ctx.JSON(200,gin.H{
-			"message":"pong",
-		})
-	})
-	r.Run()
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"myblog/routers"
+	"myblog/pkg/setting"
+)
+
+func main() {
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
